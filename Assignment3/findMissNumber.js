bbf0d8ae870545c1791f84128950e6e021cc0a12
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-05-24 07:26:52 +0530

    Update findMissNumber.js

--- Assignment3/findMissNumber.js
+++ Assignment3/findMissNumber.js
@@ -1,19 +1,15 @@
 function findMissingNumber(arr) {
    
     const n = Math.max(...arr);
-    const totalSum = (n * (n + 1)) / 2;
-  
-    const actualSum = arr.reduce((acc, num) => acc + num, 0);
-  
+    const totalSum = (n * (n + 1)) / 2;  
+    const actualSum = arr.reduce((acc, num) => acc + num, 0);  
 
     const missingNumber = totalSum - actualSum;
   
-    // If the missing number is 0, that means the missing number is n+1 (the next number in sequence)
     return missingNumber === 0 ? n + 1 : missingNumber;
   }
-  
-  // Example usage:
+  //Ex.
   const arr = [1, 2,4, 6, 3, 7, 8];
   const missingNumber = findMissingNumber(arr);
-  console.log(`The missing number is ${missingNumber}`); // Output: The missing number is 5
+  console.log(`The missing number is ${missingNumber}`); 
   
