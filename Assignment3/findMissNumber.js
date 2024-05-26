cfa5838e41a03010ce04d370569e6cb5947a3032
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-05-26 16:54:21 +0530

    Update findMissNumber.js

--- Assignment3/findMissNumber.js
+++ Assignment3/findMissNumber.js
@@ -8,7 +8,7 @@ function findMissingNumber(arr) {
   
     return missingNumber === 0 ? n + 1 : missingNumber;
   }
-  //Ex.
+  //Example 
   const arr = [1, 2,4, 6, 3, 7, 8];
   const missingNumber = findMissingNumber(arr);
   console.log(`The missing number is ${missingNumber}`); 
