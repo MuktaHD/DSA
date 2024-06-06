aa2e148a6008f138c4a7444ba874e129e1aa0aef
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-06-06 13:53:29 +0530

    Update 5Postfix.js

--- Assignment5/5Postfix.js
+++ Assignment5/5Postfix.js
@@ -3,15 +3,11 @@ function evaluatePostfix(expression) {
 
     for (let char of expression) {
         if (!isNaN(char)) {
-            // If the character is a number, push it to the stack
             stack.push(Number(char));
         } else {
-            // Otherwise, it's an operator, so pop the top two elements from the stack
             let b = stack.pop();
             let a = stack.pop();
             let result;
-
-            // Apply the operator
             switch (char) {
                 case '+':
                     result = a + b;
@@ -28,16 +24,12 @@ function evaluatePostfix(expression) {
                 default:
                     throw new Error(`Unsupported operator: ${char}`);
             }
-
-            // Push the result back to the stack
             stack.push(result);
         }
     }
 
-    // The final result should be the only element left in the stack
     return stack.pop();
 }
 
-// Test case
-console.log(evaluatePostfix("231*+9-")); // Output: -4
+console.log(evaluatePostfix("231*+9-"));
 
