c3b3708b6d30ce1dda40c809765a6ae75f3ca6d0
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-06-06 13:56:32 +0530

    Update 7PreToInfix.js

--- Assignment5/7PreToInfix.js
+++ Assignment5/7PreToInfix.js
@@ -1,28 +1,23 @@
 function prefixToInfix(prefix) {
     let stack = [];
 
-    // Iterate over the prefix expression from right to left
     for (let i = prefix.length - 1; i >= 0; i--) {
         let char = prefix[i];
 
         if (/[a-zA-Z]/.test(char)) {
-            // If the character is an operand, push it onto the stack
             stack.push(char);
         } else {
-            // If the character is an operator, pop two elements from the stack
+    
             let operand1 = stack.pop();
             let operand2 = stack.pop();
-
-            // Form a new string in infix format and push it back onto the stack
             let newExpr = `(${operand1}${char}${operand2})`;
             stack.push(newExpr);
         }
     }
 
-    // The final element in the stack is the complete infix expression
     return stack.pop();
 }
 
-// Test case
+
 const prefix = "+AB-CD";
-console.log(prefixToInfix(prefix)); // Output: "((A+B)-(C+D))"
+console.log(prefixToInfix(prefix)); 
