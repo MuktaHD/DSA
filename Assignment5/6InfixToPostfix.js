a2c25c08d5b4a42f3df99cfe93df3ecbf6d52cf3
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-06-06 13:54:42 +0530

    Update 6InfixToPostfix.js

--- Assignment5/6InfixToPostfix.js
+++ Assignment5/6InfixToPostfix.js
@@ -10,30 +10,24 @@ function infixToPostfix(infix) {
 
     let stack = [];
     let postfix = [];
-    
     for (let char of infix) {
         if (/[a-zA-Z0-9]/.test(char)) {
-            // If the character is an operand, add it to the postfix output
             postfix.push(char);
         } else if (char === '(') {
-            // If the character is '(', push it onto the stack
             stack.push(char);
         } else if (char === ')') {
-            // If the character is ')', pop and output from the stack until '(' is found
             while (stack.length > 0 && stack[stack.length - 1] !== '(') {
                 postfix.push(stack.pop());
             }
-            stack.pop(); // Pop '(' from the stack
+            stack.pop(); 
         } else {
-            // If an operator is encountered
+        
             while (stack.length > 0 && precedence[stack[stack.length - 1]] >= precedence[char]) {
                 postfix.push(stack.pop());
             }
             stack.push(char);
         }
     }
-
-    // Pop all the operators from the stack
     while (stack.length > 0) {
         postfix.push(stack.pop());
     }
@@ -41,6 +35,5 @@ function infixToPostfix(infix) {
     return postfix.join('');
 }
 
-// Test case
 const infix = "a+b*(c^d-e)^(f+g*h)-i";
-console.log(infixToPostfix(infix)); // Output: "abcd^e-fgh*+^*+i-"
+console.log(infixToPostfix(infix)); 
